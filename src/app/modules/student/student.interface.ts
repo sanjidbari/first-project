2a031894d7458e1870c8a00d0c687983c51422e1
sanjidbari <sanjidbari.com>
2024-12-19 18:59:11 -0500

    insert function done

---
+++ src/app/modules/student/student.interface.ts
@@ -0,0 +1,30 @@
+export type Guardian = {
+  fatherName: string;
+  fatherOccupation: string;
+  fatherContactNo: string;
+  motherName: string;
+  motherOccupation: string;
+  motherContactNo: string;
+};
+
+export type UserName = {
+    firstName: string;
+    middleName?: string;
+    lastName: string;
+}
+
+export type Student = {
+  id: string;
+  name: UserName;
+  gender: 'male' | 'female';
+  dOB: string;
+  email: string;
+  contactNo: string;
+  emergencyNo: string;
+  bloodGroup?: 'A+' | 'A-' | 'B+' | 'B-' | 'AB+' | 'AB-' | 'O+' | 'O-';
+  presentAddress: string;
+  permanentAddress: string;
+  guardian: Guardian;
+  profileImg?: string;
+  isActive: 'active' | 'inActive';
+};
